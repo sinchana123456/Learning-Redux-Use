76ed21c80ef584ab9fa058fbefc3108f1006be44
Sinchana Gowda <113234656+sinchana123456@users.noreply.github.com>
2023-01-21 21:49:38 +0530

    increment by 5

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
     if(action.type === 'increment'){
-        return { counter: state.counter + 1 }
+        return { counter: state.counter + 5 }
     }else {
         if(action.type === 'decrement'){
-            return { counter: state.counter - 1 }
+            return { counter: state.counter - 5 }
     }
 }
     return state;
